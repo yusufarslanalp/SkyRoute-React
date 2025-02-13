d2ab9c6913592678894bd4839e9d57948809b521
yusuf.arslanalp <yusuf.arslanalp@n11.com>
2025-02-13 11:54:37 +0300

    recenter tiele

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
 import Navbar from './Navbar';
 import Page1 from './Page1';
 import Page2 from './Page2';
@@ -9,18 +10,28 @@ const App = () => {
   return (
     <Router>
       <div>
-        <h1>Welcome To My App</h1>
-        <Navbar />
+      <h1 className="title">Welcome To My App</h1>
         
-        {/* Routing Pages */}
-        <Routes>
-          <Route path="/page1" element={<Page1 />} />
-          <Route path="/page2" element={<Page2 />} />
-          <Route path="/page3" element={<Page3 />} />
-        </Routes>
-        
-        {/* Default Content */}
-        <div>Page Content (Hello From Page Content)</div>
+        {/* Flex container for navbar and page content */}
+        <div style={{ display: 'flex' }}>
+          {/* Navbar with 3/12 width */}
+          <div style={{ width: '25%', padding: '10px' }}>
+            <Navbar />
+          </div>
+          
+          {/* Page content with 9/12 width */}
+          <div style={{ width: '75%', padding: '10px' }}>
+            {/* Routing Pages */}
+            <Routes>
+              <Route path="/page1" element={<Page1 />} />
+              <Route path="/page2" element={<Page2 />} />
+              <Route path="/page3" element={<Page3 />} />
+            </Routes>
+            
+            {/* Default Content */}
+            <div>Page Content (Hello From Page Content)</div>
+          </div>
+        </div>
       </div>
     </Router>
   );
--- src/Navbar.js
+++ src/Navbar.js
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav>
-      <ul>
-        <li>
+      <ul style={{ listStyleType: 'none', padding: 0 }}>
+        <li style={{ margin: '10px 0' }}>
           <Link to="/page1">Page-1-link</Link>
         </li>
-        <li>
+        <li style={{ margin: '10px 0' }}>
           <Link to="/page2">Page-2-link</Link>
         </li>
-        <li>
+        <li style={{ margin: '10px 0' }}>
           <Link to="/page3">Page-3-link</Link>
         </li>
       </ul>
