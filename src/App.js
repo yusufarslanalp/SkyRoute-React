7b252e671d6fa8717cd6f6e7a90d008f051232c7
yusufarslanalp <ayusufa42@gmail.com>
2025-02-16 13:21:31 +0300

    feat: print airport names using flightIndex field

--- src/App.js
+++ src/App.js
@@ -10,18 +10,14 @@ const App = () => {
   return (
     <Router>
       <div>
-      <h1 className="title">Welcome To My App</h1>
+      <h1 className="title">Welcome To SkyRoute App</h1>
         
-        {/* Flex container for navbar and page content */}
         <div style={{ display: 'flex' }}>
-          {/* Navbar with 3/12 width */}
           <div style={{ width: '25%', padding: '10px' }}>
             <Navbar />
           </div>
           
-          {/* Page content with 9/12 width */}
           <div style={{ width: '75%', padding: '10px' }}>
-            {/* Routing Pages */}
             <Routes>
               <Route path="/page1" element={<Page1 />} />
               <Route path="/page2" element={<Page2 />} />
